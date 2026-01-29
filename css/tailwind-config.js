08b63279e79d7db620bcbe036dca6492829b0f6b
mahani_inventori <mahani.inventori@gmail.com>
2026-01-29 10:13:36 +0700

    Create tailwind-config.js

---
+++ css/tailwind-config.js
@@ -0,0 +1,14 @@
+      tailwind.config = {
+        darkMode: "class",
+        theme: {
+          extend: {
+            colors: {
+              primary: {
+                900: "#1e3a8a", // biru tua sidebar
+                800: "#1e40af",
+                700: "#1d4ed8",
+              },
+            },
+          },
+        },
+      };
